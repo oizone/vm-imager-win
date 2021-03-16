a241063d9c66e805de7ddcb9230cf3ef03f9cbde
Onni Rautanen <oizone@oizone.net>
2021-03-16 11:40:21 +0200

    first version

---
+++ build-win.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+MD5=`md5sum $1 | cut -d" " -f 1`
+OS_CHECKSUM="os_iso_checksum=${MD5}"
+OS_ISO="os_iso_path=${1}"
+OS_VER=`echo $1| cut -d "_" -f 4`
+OS_VERSION=`echo $1|cut -d"_" -f 5-7`
+
+TOOLURL="https://packages.vmware.com/tools/releases/latest/windows/x64/"
+TOOLFILE=`curl ${TOOLURL}| grep -oP 'HREF="\K[^"]*' |tail -1`
+
+if ! curl -o ${TOOLFILE} ${TOOLURL}${TOOLFILE}; then
+   exit 1
+fi
+
+ln -sf ${TOOLFILE} setup64.exe
+TOOLS_VERSION=`echo $TOOLFILE|cut -d "-" -f 3`
+
+#mkdir -p ova
+for OS in autounattend/${OS_VER}/*; do
+    OSNAME="`echo $OS | cut -d "/" -f 3`"
+    OSVAR="vm-name=${OSNAME}"
+    EXPORT_NAME="vsphere-export-name=`echo $OSNAME| sed 's/ /_/g'`-${OS_VERSION}-VMtools-${TOOLS_VERSION}"
+    EXPORT_FOLDER="vsphere-export-folder=./ovf-export/${OS_VER}"
+    echo $EXPORT_NAME
+    if ! ../packer build -var-file=credentials.json -var "${OS_ISO}" -var "${OS_CHECKSUM}" -var "${OSVAR}" -var "${EXPORT_FOLDER}" -var "${EXPORT_NAME}" -force windows${OS_VER}.json; then
+	exit 1
+    fi
+    #cd ovf-export
+    #TARNAME="`echo $OSNAME|sed 's/ /_/g'`"
+    #if ! tar -cf "../ova/${TARNAME}-${OS_VERSION}-${TOOLS-VERSION}.ova" "$OSNAME"; then
+	#exit 1
+    #fi
+    #cd ..
+done
+
+mkdir -p windows/${OS_VER}
+mv ovf-export/${OS_VER}/* windows/${OS_VER}/
+
+#if ! python3 ../make_vcsp_2018.py -n windows -t local -p windows/${OS_VER}; then
+#    exit 1
+#fi
+
+#../azcopy_linux_amd64_10.9.0/azcopy cp windows/${OS_VER} "https://${STORAGE_ACCOUNT}.blob.core.windows.net/${STORAGE_CONTAINER}/windows/?${SAS}" --recursive=true
+
+#if ! python uploads3.py windows/${OS_VER}; then
+#    exit 1
+#fi
+
+#if ! python3 ../make_vcsp_2018.py -n hiaas-vmimage-dev -t s3 -p hiaas-vmimage-dev; then
+#    exit 1
+#fi
