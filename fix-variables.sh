2e9b7234347ddd5f002c3863a81b9adaf8e64b4c
Onni Rautanen <oizone@oizone.net>
2021-03-16 12:54:48 +0200

    variable name fix

--- fix-variables.sh
+++ fix-variables.sh
@@ -2,17 +2,17 @@
 VCENTER_NAME=""
 VCENTER_USER=""
 VCENTER_PASSWORD=""
-2016_STANDARD_KEY=""
-2016_ENTERPRISE_KEY=""
-2019_STANDARD_KEY=""
-2019_ENTERPRISE_KEY=""
+STANDARD_2016_KEY=""
+ENTERPRISE_2016_KEY=""
+STANDARD_2019_KEY=""
+ENTERPRISE_2019_KEY=""
 
 sed -i ´s/###VCENTER_NAME###/${VCENTER_NAME}/´ credentials.json
 sed -i ´s/###VCENTER_USER###/${VCENTER_USER}/´ credentials.json
 sed -i ´s/###VCENTER_PASSWORD###/${VCENTER_PASSWORD}/´ credentials.json
 
-find autounattend/2016 -type f -exec sed -i `` -e 's/###STANDARD_KEY###/${2016_STANDARD_KEY}/g' {} \;
-find autounattend/2016 -type f -exec sed -i `` -e 's/###DATACENTER_KEY###/${2016_ENTERPRISE_KEY}/g' {} \;
-find autounattend/2019 -type f -exec sed -i `` -e 's/###STANDARD_KEY###/${2019_STANDARD_KEY}/g' {} \;
-find autounattend/2019 -type f -exec sed -i `` -e 's/###DATACENTER_KEY###/${2019_ENTERPRISE_KEY}/g' {} \;
+find autounattend/2016 -type f -exec sed -i 's/###STANDARD_KEY###/${STANDARD_2016_KEY}/g' {} \;
+find autounattend/2016 -type f -exec sed -i 's/###DATACENTER_KEY###/${ENTERPRISE_2016_KEY}/g' {} \;
+find autounattend/2019 -type f -exec sed -i 's/###STANDARD_KEY###/${STANDARD_2019_KEY}/g' {} \;
+find autounattend/2019 -type f -exec sed -i 's/###DATACENTER_KEY###/${ENTERPRISE_2019_KEY}/g' {} \;
 
